1ff95141c5c5d189c03955eaddf53b5ba690ccd7
Nícolas Iensen <nicolas@iensen.me>
2014-01-07 09:25:57 -0200

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Multitude::Application.config.secret_key_base = 'e83111e57c9d2651e3ec3a5cb372051073f8ad12034a9e6fb6614421618400c9de8502cb32951b179f24a9356cf4af3a055c5900623a7b0bf8a8af362cc273fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Multitude::Application.config.session_store :cookie_store, key: '_multitude_session'
