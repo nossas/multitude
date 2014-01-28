d21cad8568f22ee460ffa82a27c2c28263a445bf
Nícolas Iensen <nicolas@iensen.me>
2014-01-28 13:48:29 -0200

    100 years for session expiration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br'
+  Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
 else
-  Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session'
+  Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
 end
