5d4ae0f71fa198974fd81a626fa650553580f49e
Josias Schneider <josias.ds@gmail.com>
2014-01-29 15:13:57 -0200

    [#64732432] add staging.rb file; add staging config to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,8 @@
 
 if Rails.env.production?
   Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio.org.br', expire_after: 100.years
+elsif Rails.env.staging?
+  Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', domain: 'meurio-staging.org.br', expire_after: 100.years
 else
   Multitude::Application.config.session_store :cookie_store, key: '_meurio_accounts_session', expire_after: 100.years
 end
