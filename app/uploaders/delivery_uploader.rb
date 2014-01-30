1218e62cb828c4a493ab0210b3b3d528f7573fb3
Nícolas Iensen <nicolas@iensen.me>
2014-01-30 14:46:23 -0200

    Configure AWS for carrier wave

--- app/uploaders/delivery_uploader.rb
+++ app/uploaders/delivery_uploader.rb
@@ -11,6 +11,7 @@ class DeliveryUploader < CarrierWave::Uploader::Base
     storage :fog
   else
     storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
