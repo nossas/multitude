c388095994f6f402819b95f01cb5910a214bd95f
Nícolas Iensen <nicolas@iensen.me>
2014-01-30 15:09:34 -0200

    Add host option for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Multitude::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  default_url_options[:host] = "multitude.meurio.org.br"
 end
