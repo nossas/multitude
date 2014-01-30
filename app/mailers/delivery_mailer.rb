99b6cea68fca6808f53864e0e26c2ba03fb2065a
Nícolas Iensen <nicolas@iensen.me>
2014-01-30 15:16:56 -0200

    Add header to the mailers

--- app/mailers/delivery_mailer.rb
+++ app/mailers/delivery_mailer.rb
@@ -2,6 +2,7 @@ class DeliveryMailer < ActionMailer::Base
   default from: "Meu Rio <contato@meurio.org.br>"
   
   def new_delivery delivery
+    headers "X-SMTPAPI" => "{ \"category\": [\"multitude\", \"new_delivery\"] }"
     @delivery = delivery
     mail(to: delivery.task.user.email, subject: "Sua tarefa recebeu uma entrega")
   end
--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -2,6 +2,7 @@ class TaskMailer < ActionMailer::Base
   default from: "Meu Rio <contato@meurio.org.br>"
 
   def match user, task
+    headers "X-SMTPAPI" => "{ \"category\": [\"multitude\", \"match\"] }"
     mail(to: user.email, subject: "Uma nova tarefa perfeita para você")
   end
 end
