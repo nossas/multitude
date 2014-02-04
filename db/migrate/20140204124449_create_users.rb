d013b514224342cdec6ed6badf17719a1410c3ea
Nícolas Iensen <nicolas@iensen.me>
2014-02-04 10:50:23 -0200

    Fix migration

--- db/migrate/20140204124449_create_users.rb
+++ db/migrate/20140204124449_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def up
     if Rails.env.production? || Rails.env.staging?
       execute ""\
-        "CREATE FOREIGN TABLE users(id integer NOT NULL, email character varying(255) DEFAULT ''::character varying NOT NULL, first_name character varying(255) NOT NULL, last_name character varying(255) NOT NULL, avatar character varying(255), skills character varying(255)[] DEFAULT '{}'::character varying[])"\
-        "SERVER meurio_accounts"\
+        "CREATE FOREIGN TABLE users(id integer NOT NULL, email character varying(255) DEFAULT ''::character varying NOT NULL, first_name character varying(255) NOT NULL, last_name character varying(255) NOT NULL, avatar character varying(255), skills character varying(255)[] DEFAULT '{}'::character varying[]) "\
+        "SERVER meurio_accounts "\
         "OPTIONS (table_name 'users');"
     else
       execute "CREATE TABLE users(id integer NOT NULL, email character varying(255) DEFAULT ''::character varying NOT NULL, first_name character varying(255) NOT NULL, last_name character varying(255) NOT NULL, avatar character varying(255), skills character varying(255)[] DEFAULT '{}'::character varying[])"
