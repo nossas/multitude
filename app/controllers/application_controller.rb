429623dd2a4cb560103d2523a09ec902342ea94c
Nícolas Iensen <nicolas@iensen.me>
2014-02-04 19:24:50 -0200

    Fix subscribe a task with no user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,24 +7,13 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     if controller_name == "task_subscriptions" && action_name == "create"
-      session[:multitude_task_id] = @task.id
-      puts "session[:multitude_task_id] is #{session[:multitude_task_id]}"
-      redirect_to "#{ENV["ACCOUNTS_HOST"]}?redirect_url=#{root_url}"
+      redirect_to "#{ENV["ACCOUNTS_HOST"]}?redirect_url=#{create_from_session_task_task_subscriptions_url(task_id: @task.id)}"
     else
       redirect_to root_url, :alert => exception.message
     end
   end
 
-  before_filter :check_precreate
-
   def current_user
     @current_user ||= User.find_by_id(session[:ssi_user_id])
-  end
-
-  def check_precreate
-    puts "In check_precreate session[:multitude_task_id] is #{session[:multitude_task_id]}"
-    if session[:task_id]
-      redirect_to create_from_session_task_task_subscriptions_path(task_id: session[:task_id])
-    end
   end
 end
