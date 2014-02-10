3f1f0a7398df80cea16642d939a30ad7030e65fd
Nícolas Iensen <nicolas@engage.is>
2014-02-10 14:58:26 -0200

    Fix tasks#index loader

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -6,13 +6,13 @@ var availableTasksPage = 0;
 function loadAvailableTasks(){
   availableTasksPage++;
   $("a#load_more_available_tasks_button").hide();
-  $("#available_tasks .loader").show();
+  $(".load_more_available_tasks .loader").show();
   $.get("/tasks?page=" + availableTasksPage, function(data) { 
     $("#available_tasks").append(data);
     if($("#available_tasks").data("count") <= $("#available_tasks .task").length)
       $("a#load_more_available_tasks_button").remove();
     $("a#load_more_available_tasks_button").show();
-    $("#available_tasks .loader").hide();
+    $(".load_more_available_tasks .loader").hide();
   });
 } 
 
