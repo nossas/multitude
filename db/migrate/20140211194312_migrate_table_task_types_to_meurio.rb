44b865a4ca9a8caec241d4f07456cc4e236e30d5
Nícolas Iensen <nicolas@engage.is>
2014-02-11 18:44:26 -0200

    Fix migration

--- db/migrate/20140211194312_migrate_table_task_types_to_meurio.rb
+++ db/migrate/20140211194312_migrate_table_task_types_to_meurio.rb
@@ -2,7 +2,7 @@ class MigrateTableTaskTypesToMeurio < ActiveRecord::Migration
   def up
     if Rails.env.production? || Rails.env.staging?
       execute "DROP TABLE IF EXISTS task_types CASCADE;"
-      execute "CREATE FOREIGN TABLE task_types(id integer, name character varying(255), points: integer) SERVER meurio OPTIONS (table_name 'task_types');"
+      execute "CREATE FOREIGN TABLE task_types(id integer, name character varying(255), points integer) SERVER meurio OPTIONS (table_name 'task_types');"
     end
   end
 
