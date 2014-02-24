ddd54fdf2e90ec53965adf93553a561a82242fa0
Josias Schneider <josias.ds@gmail.com>
2014-02-24 11:55:51 -0300

    remove rspec warning: update :should syntax to :expect

--- spec/controllers/task_subscriptions_controller_spec.rb
+++ spec/controllers/task_subscriptions_controller_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe TaskSubscriptionsController do
   describe "#POST create" do
     context "when I'm not logged in" do
-      before { Task.stub(:find).with("1").and_return(mock_model(Task, id: 1, task_subscriptions: double(:task_subscriptions, build: true))) }
-      before { post :create, task_id: 1 }
-      it { should redirect_to "#{ENV["ACCOUNTS_HOST"]}?redirect_url=#{create_from_session_task_task_subscriptions_url(task_id: 1)}" }
+      before { allow(Task).to receive(:find).and_return(mock_model(Task, id: 1, task_subscriptions: double(:task_subscriptions, build: true))) }
+      
+      it "redirects to login" do
+        post :create, task_id: 1
+        expect(response).to redirect_to "#{ENV["ACCOUNTS_HOST"]}?redirect_url=#{create_from_session_task_task_subscriptions_url(task_id: 1)}"
+      end
     end
   end
 end
