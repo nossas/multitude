16f92c46ab43a69607355c5fb85280966b555e3d
Nícolas Iensen <nicolas@engage.is>
2014-02-25 11:55:22 -0300

    Create Delivery#sync_reward

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -33,10 +33,17 @@ class Delivery < ActiveRecord::Base
   def accept!
     self.update_attribute :accepted_at, Time.now
     MultitudeMailer.delay.your_delivery_was_accepted(self)
-    HTTParty.post("#{ENV["MEURIO_HOST"]}/rewards.json", body: {token: ENV["MEURIO_API_TOKEN"], reward: {task_type_id: self.task.task_type.id, user_uid: self.user.id, points: task.points}})
+    sync_reward
   end
 
   def reject!
     self.update_attribute :rejected_at, Time.now
+  end
+
+  def sync_reward
+    url = "#{ENV["MEURIO_HOST"]}/rewards.json"
+    reward = {task_type_id: self.task.task_type.id, user_uid: self.user.id, points: task.points}
+    body = { token: ENV["MEURIO_API_TOKEN"], reward: reward }
+    puts HTTParty.post(url, body: body)
   end
 end
