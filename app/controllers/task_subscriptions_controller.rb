56c8cf70bce0cead8d4f713a23ea45eea8e66f41
Nícolas Iensen <nicolas@engage.is>
2014-03-07 09:36:19 -0300

    Change redirect after create the task subscription

--- app/controllers/task_subscriptions_controller.rb
+++ app/controllers/task_subscriptions_controller.rb
@@ -6,7 +6,7 @@ class TaskSubscriptionsController < InheritedResources::Base
     resource.task = @task
     resource.user = current_user
     create! do |success, failure|
-      success.html { redirect_to task_path(@task) }
+      success.html { redirect_to task_path(@task, anchor: "created") }
       failure.html { redirect_to task_path(@task) }
     end
   end
