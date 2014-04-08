4648d166efa335058f5c13caab6619c31768818f
Nícolas Iensen <nicolas@iensen.me>
2014-04-08 08:44:40 -0300

    Fix migration

--- db/migrate/20140408113058_alter_server_meurio_accounts.rb
+++ db/migrate/20140408113058_alter_server_meurio_accounts.rb
@@ -3,11 +3,11 @@ class AlterServerMeurioAccounts < ActiveRecord::Migration
     if Rails.env.production? || Rails.env.staging?
       execute ""\
       "ALTER SERVER meurio_accounts "\
-      "OPTIONS (host '#{ENV["MEURIO_ACCOUNTS_DBHOST"]}', dbname '#{ENV["MEURIO_ACCOUNTS_DBNAME"]}')"
+      "OPTIONS (SET host '#{ENV["MEURIO_ACCOUNTS_DBHOST"]}', SET dbname '#{ENV["MEURIO_ACCOUNTS_DBNAME"]}', ADD port '#{ENV["MEURIO_ACCOUNTS_DBPORT"]}')"
 
       execute ""\
       "ALTER USER MAPPING FOR #{ENV["DB_USERNAME"]} "\
-      "SERVER meurio_accounts OPTIONS (user '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}', password '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}');"
+      "SERVER meurio_accounts OPTIONS (SET user '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}', SET password '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}');"
     end
   end
 end
