32350336ad33825a33974fd65f4fa83801819f76
Nícolas Iensen <nicolas@iensen.me>
2014-07-30 13:35:18 -0300

    Add support for CAS sign out

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-# Be sure to restart your server when you modify this file.
-Multitude::Application.config.session_store :cookie_store, key: '_multitude_session'
+require 'rack-cas/session_store/rails/active_record'
+Multitude::Application.config.session_store :rack_cas_active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703192342) do
+ActiveRecord::Schema.define(version: 20140730163438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,18 @@ ActiveRecord::Schema.define(version: 20140703192342) do
     t.string "city"
     t.text   "email_signature_html"
   end
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.string   "cas_ticket"
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["cas_ticket"], name: "index_sessions_on_cas_ticket", using: :btree
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "task_subscriptions", force: true do |t|
     t.integer  "user_id",    null: false
