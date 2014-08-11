dd2c437335955c4538e22efe803012d744b9b5d4
Josias Schneider <josias.ds@gmail.com>
2014-08-11 16:58:04 -0300

    [#76326482] fix bug on task skills

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,7 @@ class TasksController < InheritedResources::Base
   # TODO: the check_box helper is adding a hidden skill option with no value and it
   # is been submitted to the create and update actions. We have to figure out what's
   # the best way to supress this hidden checkbox, and then remove this before filter
+  # UPDATE: Rails team thinks this is the desirable behaviour (https://github.com/rails/rails/issues/12605)
   before_action(only: [:create, :update]) do
     params[:task][:skills].delete("")
   end
@@ -30,6 +31,7 @@ class TasksController < InheritedResources::Base
 
   def create
     resource.user = current_user
+    resource.skills = params[:task][:skills]
     create!
   end
 
