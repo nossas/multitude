663a8703a7ce3ad5d6394493ba79395f79909a96
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-12-02 15:05:12 -0200

    [#82680760] fix staging migration

--- db/migrate/20141103133025_create_user_mapping_for_new_db_username.rb
+++ db/migrate/20141103133025_create_user_mapping_for_new_db_username.rb
@@ -1,6 +1,6 @@
 class CreateUserMappingForNewDbUsername < ActiveRecord::Migration
   def up
-    if Rails.env.production? || Rails.env.staging?
+    if Rails.env.production?
       execute "CREATE USER MAPPING for #{ENV["DB_USERNAME"]} SERVER meurio OPTIONS (user '#{ENV["MEURIO_DBUSER"]}', password '#{ENV["MEURIO_DBPASS"]}');"
       execute "CREATE USER MAPPING for #{ENV["DB_USERNAME"]} SERVER meurio_accounts OPTIONS (user '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}', password '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}');"
     end
