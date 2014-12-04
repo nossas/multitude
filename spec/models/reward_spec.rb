a4170f564262d7619f465515197011e8c883ca14
Josias Schneider <josias.ds@gmail.com>
2014-12-04 14:14:14 -0200

    [#82680760] add Reward#sync_reward on after_create

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
 describe Reward do
+  before { Reward.make }
+
   it { should belong_to :user }
   it { should belong_to :task }
   it { should validate_uniqueness_of(:task_id).scoped_to(:user_id) }
+
+  describe "#sync_reward" do
+    subject { Reward.make! }
+
+    context "when the request returns 201" do
+      before(:all) { stub_request(:post, "#{ENV["MEURIO_HOST"]}/rewards.json").to_return(status: 201) }
+      it "should update the rewarded flag to true" do
+        expect(subject).to receive(:update_attribute).with(:rewarded, true)
+        subject.sync_reward
+      end
+    end
+
+    context "when the request doesn't returns 201" do
+      before(:all) { stub_request(:post, "#{ENV["MEURIO_HOST"]}/rewards.json").to_return(status: 403) }
+      it "should not update the rewarded flag to true" do
+        expect(subject).not_to receive(:update_attribute).with(:rewarded, true)
+        subject.sync_reward
+      end
+    end
+  end
 end
