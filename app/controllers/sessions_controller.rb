ce02b895f4ca2747c565e22bb1989bb797f82f27
Filipe Abreu <filabreu@gmail.com>
2016-09-21 18:49:57 -0300

    Skip CSRF authenticity for SessiosController.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     @current_user = @user
